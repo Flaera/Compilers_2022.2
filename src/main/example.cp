2fa9f247450a243954c548558f4bcb8f9a11c2ad
Flaera <rafael2mcont@gmail.com>
2023-02-17 23:40:39 -0300

    Partial implementation of tokeninzing.

--- src/main/example.cp
+++ src/main/example.cp
@@ -11,5 +11,5 @@ begin
 end
 
 
-printName();
+printname();
 .
--- src/main/main.c
+++ src/main/main.c
@@ -13,12 +13,13 @@
 
 
 //classes of globals tokens
-char PR[11][BIG_INT] = {
+char PR[13][BIG_INT] = {
     "program\0", 
     "integer\0", "real\0", "boolean\0",
-    "var\0", "fifo_of_integer\0", "fifo_of_real\0",
+    "var:\0", "fifo_of_integer\0", "fifo_of_real\0",
     "procedure\0", "function\0",
-    "concatena_fifo\0", "inverte_fifo\0"
+    "concatena_fifo\0", "inverte_fifo\0",
+    "write\0", "read\0"
 };
 char DS[7][2] = {
     ";\0",".\0",",\0","(\0",")\0","[\0","]\0"
@@ -33,6 +34,8 @@ char REL_OPERATORS[6][3] = {
     "=\0",">\0","<\0",">=\0","<=\0","<>\0"
 };
 char VALUES_BOLLEAN[3][6] = {"true\0", "false\0", "empty\0"};
+char DIGITO[10][2] = {"0\0","1\0","2\0","3\0","4\0","5\0","6\0","7\0","8\0","9\0"};
+char DIGIT[9][2] = {"1\0","2\0","3\0","4\0","5\0","6\0","7\0","8\0","9\0"};
 
 int acc_adress = 0;
 
@@ -64,7 +67,6 @@ Node* insertFifo(Node *fifo, char* new_value, char* new_type,
         new_node->sentence = new_sentence;
         new_node->adress = acc_adress;
         new_node->line = new_line;
-        new_node->next = NULL;
         if(fifo==NULL){
             fifo = new_node;
             acc_adress++;
@@ -152,7 +154,19 @@ int compareString(char* str0, char* str1){
     while (str0[acc]==str1[acc] && str0[acc]!='\0' && str1[acc]!='\0'){
         acc++;
     }
-    if (str0[acc]=='\0'&&str1[acc]=='\0'){return 1;}
+    if (str0[acc]=='\0' && str1[acc]=='\0'){
+        // DEBUG3{printf("part-c:str0:%d, str1:%d\n",str0[acc],str1[acc]);}
+        return 1;}
+    // int acc1 = 0;
+    // while (str0[acc]!='\0' && str1[acc1]!='\0'){
+    //     if (str0[acc]!=str1[acc] || acc!=acc1){return 1;}
+    //     if (str0[acc]!='\0'){acc++;}
+    //     if (str1[acc]!='\0'){acc1++;}
+    // }
+    // if (str0[acc]=='\0' && str1[acc]=='\0'){
+    //     DEBUG3{printf("part-c:str0:%d, str1:%d\n",str0[acc],str1[acc]);}
+    //     return 1;
+    // }
     return 0;
 }
 
@@ -176,6 +190,16 @@ void invertString(char str[]){
 }
 
 
+char* toString(char i, char* string){
+    char* str = malloc(BIG_INT);
+    str[0]=i;
+    int acc = 1;
+    while(string[acc]!='\0'){str[acc+1]=string[acc];acc++;}
+    str[acc+1]='\0';
+    return str;
+}
+
+
 void programErrorMessageAnaLex(int curr_line){
     printf("Erro de sintaxe no programa. Linha: %d.\n", curr_line);
 }
@@ -183,15 +207,17 @@ void programErrorMessageAnaLex(int curr_line){
 
 int isDS(char str[]){
     for (int i=0; i<7; ++i){
-        if (compareString(str,DS[i])==1){return 1;}
+        if (compareString(str,DS[i])){return 1;}
     }
     return 0;
 }
 
 
 int isPR(char str[]){
-    for (int i=0; i<11; ++i){
-        if (compareString(str,PR[i])==1){return 1;}
+    for (int i=0; i<13; ++i){
+        // DEBUG3{printf("AQUI MESMO!!!%s-%s\n", PR[i], str);}
+        if (compareString(str,PR[i])){return 1;}
+        // DEBUG3{printf("AQUI N DÀ\n");}
     }
     return 0;
 }
@@ -205,22 +231,56 @@ int isDC(char str[]){
 }
 
 
+int isMO(char str[]){
+    for (int i=0; i<6; ++i){
+        if (compareString(str, MATH_OPERATORS[i])){return 1;}
+    }
+    return 0;
+}
+
+
+int isRO(char str[]){
+    for (int i=0; i<6; ++i){
+        if (compareString(str, REL_OPERATORS[i])){return 1;}
+    }
+    return 0;
+}
+
+
 void showTokens(Node* tokens){
     printf("TOKENS FIFO:\n");
     Node* aux = tokens;
+    
     while (aux->next!=NULL){
         if (aux->error[0]=='N'){
-            printf("## %s|%s|%d|%d|%d ## \n", aux->value, aux->type,
-             aux->adress, aux->line, aux->sentence);
-        }
-        else{
-            printf("## %s|%s|%d|%d|%d ## | ERRO!  \n", aux->value, aux->type,
-             aux->adress, aux->line, aux->sentence);
+            printf("## valor:%s|tipo:%s|endereco:%d|linha:%d|sentenca:%d ##\n",
+                aux->value, aux->type,
+                aux->adress, aux->line, aux->sentence);
         }
         aux = aux->next;
     }
+    if (aux->error[0]=='N'){
+        printf("## valor:%s|tipo:%s|endereco:%d|linha:%d|sentenca:%d ##\n",
+            aux->value, aux->type,
+            aux->adress, aux->line, aux->sentence);
+    }
 
-    free(tokens);
+    aux = tokens;
+    while (aux->next!=NULL){
+        if (aux->error[0]=='Y'){
+            printf("## valor:%s|tipo:%s|endereco:%d|linha:%d|sentenca:%d ## | ERRO! \n",
+                aux->value, aux->type,
+                aux->adress, aux->line, aux->sentence);
+        }
+        aux = aux->next;
+    }
+    if (aux->error[0]=='Y'){
+        printf("## valor:%s|tipo:%s|endereco:%d|linha:%d|sentenca:%d ## | ERRO! \n",
+            aux->value, aux->type,
+            aux->adress, aux->line, aux->sentence);
+    }
+
+    free(aux);
 }
 
 
@@ -247,6 +307,7 @@ int main(){
     char* part = malloc(BIG_INT);
     int i = acc-2;
     while (i>=0){
+        DEBUG3{printf("sequence:%c\n", sequence_cp[i]);}
         if (sequence_cp[i]=='\n'){current_line++;}
         if (sequence_cp[i]==';'){curr_sentence++;}
 
@@ -254,30 +315,26 @@ int main(){
          || sequence_cp[i]=='(' || sequence_cp[i]==')' || sequence_cp[i]=='['
           || sequence_cp[i]==']'){
             part[0]=sequence_cp[i];part[1]='\0';
+            DEBUG3{printf("partDS:%s-\n", part);}
             tokens=insertFifo(tokens,part,"DS\0",current_line,curr_sentence,"NO\0");
-            acc_adress++;
             i--;
         }
-        else if (sequence_cp[i]==' ' || sequence_cp[i]=='\t' || sequence_cp[i]=='\n'){
-            int j = i-1;
+        else if (sequence_cp[i]=='f' && sequence_cp[i-1]>=48 && sequence_cp[i-1]<=57){
+            tokens=insertFifo(tokens,"f\0","PR\0",current_line,curr_sentence,"NO\0");
+            i--;
+        }
+        else if ((sequence_cp[i]>=97 && sequence_cp[i]<=122)){
+            int j = i;
             int l = 0;
-            // DEBUG3{printf("AQUI\n");}
-            while ((sequence_cp[j]>=97 && sequence_cp[j]<=122
-             && sequence_cp[i]!=';' && sequence_cp[i]!=',' && sequence_cp[i]!='.'
-              && sequence_cp[i]!='(' && sequence_cp[i]!=')' && sequence_cp[i]!='['
-               && sequence_cp[i]!=']' && sequence_cp[i]!='\"')){
+            while ((sequence_cp[j]>=97 && sequence_cp[j]<=122)){
                 part[l] = sequence_cp[j];
                 j--;
                 l++;
-                // DEBUG3{printf("AQUI!!!!\n");}
             }
-            part[l]='\0';
             invertString(part);
-            DEBUG3{printf("part:%s-%d-%d-\n", part, j, l);}
-            if (isDS(part)){
-                tokens=insertFifo(tokens,part,"DS\0",current_line,curr_sentence,"NO\0");
-            }
-            else if (isPR(part)){
+            DEBUG3{printf("partPR-DC:%s-\n", part);}
+
+            if (isPR(part)){
                 tokens=insertFifo(tokens,part,"PR\0",current_line,curr_sentence,"NO\0");
             }
             else if (isDC(part)){
@@ -285,17 +342,90 @@ int main(){
             }
             i = i-l-1;
         }
+        else if (sequence_cp[i]==':' && sequence_cp[i-1]=='r' && sequence_cp[i-2]=='a'
+         && sequence_cp[i-3]=='v'){
+            // part = "var:\0";
+            DEBUG3{printf("partPRvar:%s-\n", part);}
+            tokens=insertFifo(tokens,part,"PR\0",current_line,curr_sentence,"NO\0");
+            i=i-5;
+        }
+        else if (sequence_cp[i]==37||sequence_cp[i]==42
+        
+            ||sequence_cp[i]==187||sequence_cp[i]==188
+            ||sequence_cp[i]==191||sequence_cp[i]==192||sequence_cp[i]==195
+            ||sequence_cp[i]==197||sequence_cp[i]==200||sequence_cp[i]==201
+            ||sequence_cp[i]==206||sequence_cp[i]==217||sequence_cp[i]==218
+            ||sequence_cp[i]==43
+            
+            ||sequence_cp[i]==45
+            ||sequence_cp[i]==193||sequence_cp[i]==194||sequence_cp[i]==196
+            ||sequence_cp[i]==202||sequence_cp[i]==203||sequence_cp[i]==205
+            
+            ||sequence_cp[i]==47
+            ||compareString(toString(sequence_cp[i],"/\0"),"//\0")
+            ||compareString(toString(sequence_cp[i],"=\0"),":=\0")){
+            int j = i;
+            int l = 0;
+            while ((sequence_cp[i]==37||sequence_cp[i]==42||sequence_cp[i]==43
+            ||sequence_cp[i]==45||sequence_cp[i]==47)
+            ||compareString(toString(sequence_cp[i],"/\0"),"//\0")
+            ||compareString(toString(sequence_cp[i],"=\0"),":=\0")){
+                part[l] = sequence_cp[j];
+                j--;
+                l++;
+            }
+            part[l]='\0';
+            invertString(part);
+            DEBUG3{printf("partMO:%s-\n", part);}
+
+            if (isMO(part)){
+                tokens=insertFifo(tokens,part,"MO\0",current_line,curr_sentence,"NO\0");
+            }
+            else{
+                tokens=insertFifo(tokens,part,"ID\0",current_line,curr_sentence,"YES\0");
+            }
+            i = i-l-1;
+        }
+        else if (sequence_cp[i]=='='||sequence_cp[i]=='<'||sequence_cp[i]=='>'
+        ||compareString(toString(sequence_cp[i],"=\0"),">=\0")
+        ||compareString(toString(sequence_cp[i],"=\0"),"<=\0")
+        ||compareString(toString(sequence_cp[i],">\0"),"<>\0")
+        )
+        {
+            int j = i;
+            int l = 0;
+            while (sequence_cp[i]=='='||sequence_cp[i]=='<'||sequence_cp[i]=='>'
+            ||compareString(toString(sequence_cp[i],"=\0"),">=\0")
+            ||compareString(toString(sequence_cp[i],"=\0"),">=\0")
+            ||compareString(toString(sequence_cp[i],">\0"),"<>\0")){
+                part[l] = sequence_cp[j];
+                j--;
+                l++;
+            }
+            part[l]='\0';
+            invertString(part);
+            DEBUG3{printf("partRO:%s-\n", part);}
+
+            if (isRO(part)){
+                tokens=insertFifo(tokens,part,"RO\0",current_line,curr_sentence,"NO\0");
+            }
+            else{
+                tokens=insertFifo(tokens,part,"ID\0",current_line,curr_sentence,"YES\0");
+            }
+            i = i-l-1;
+        }
         else{
             // DEBUG3{printf("-%d-\n", sequence_cp[i]);}
             programErrorMessageAnaLex(current_line);
-            insertFifo(tokens,part,"ERRO\0",current_line,curr_sentence,"YES\0");
-            i--;
+            tokens = insertFifo(tokens,part,"ERRO\0",current_line,curr_sentence,"YES\0");
+            i=i-1;
         }
+        part[0]='\0';
         acc_adress++;
     }
     free(part);
 
-    // showTokens(tokens);
+    showTokens(tokens);
 
     return 0;
 }
